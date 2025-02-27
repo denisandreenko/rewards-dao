b4fe761777309a2ee5b41c79573e476593dfac97
Dzianis Andreyenka <andreenkodn@gmail.com>
2025-02-27 09:27:25 +0100

    USDC local setup
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,16 @@
+export const RWD_DECIMALS = 6;
+export const RWD_PER_USDC = 100;
+export const TOKEN_2022_SEED = "token-2022";
+export const FEES_SEED = "fees";
+export const USDC_SEED = "usdc";
+
+// Mainnet - EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
+// Devnet - 4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU
+export const USDC_MINT_ADDRESS = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+
 export const metadata = {
     name: "Rewards Token",
     symbol: "RWD",
     uri: "https://devnet.irys.xyz/71M9GquPesJ9LyiGiJKFxveood8kY6GqHP92GS2YvQrE",
-    decimals: 6,
-  }
-
-  export const TOKEN_2022_SEED = "token-2022";
-  export const FEES_SEED = "fees";
-  export const USDC_SEED = "usdc";
-  export const USDC_MINT_ADDRESS_MAINNET = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
-  export const USDC_MINT_ADDRESS_DEVNET = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
+    decimals: RWD_DECIMALS,
+}
